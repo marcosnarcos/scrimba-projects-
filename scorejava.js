84bcfb533e82e9b8850455c9513b77229456a144
marcosnarcos <wizzbueze@gmail.com>
2022-10-28 20:31:45 +0100

    Commit initial files

---
+++ scorejava.js
@@ -0,0 +1,30 @@
+let Homescore = document.querySelector(".homescore");
+let awayscore = document.querySelector(".awayscore");
+
+home = 0
+       
+// HOME FUNCTION
+function add1point() {
+	Homescore.innerHTML =home + 1
+};
+
+function add2points() {
+	Homescore.innerHTML= home + 2
+};
+
+function add3points() {
+	Homescore.innerHTML= home + 3
+};
+// AWAY FUNCTION
+function addapoint() {
+	awayscore.innerHTML= home + 1
+};
+
+function addtwopoints() {
+	awayscore.innerHTML= home + 2
+};
+
+function addthreepoints() {
+	awayscore.innerHTML= home + 3
+};
+
